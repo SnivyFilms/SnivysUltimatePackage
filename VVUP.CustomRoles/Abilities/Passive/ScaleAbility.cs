ed04ce2da26b4e77581fb0c33fcd4b1f0585b2eb
Snivy Films <Snivy.Films@gmail.com>
2025-09-27 10:24:26 -0400

    Scale thing?

--- VVUP.CustomRoles/Abilities/Passive/ScaleAbility.cs
+++ VVUP.CustomRoles/Abilities/Passive/ScaleAbility.cs
@@ -19,14 +19,17 @@ namespace VVUP.CustomRoles.Abilities.Passive
         protected override void AbilityAdded(Player player)
         {
             Log.Debug($"VVUP Custom Abilities: Scale Ability, Adding Scale Ability to {player.Nickname}, Scale: {ScaleForPlayers}");
-            PlayersWithScaleAbility.Add(player);
-            Timing.CallDelayed(2.5f, () => player.Scale = ScaleForPlayers);
+            Timing.CallDelayed(2.5f, () =>
+            {
+                player.Scale = ScaleForPlayers;
+                player.SetScale(ScaleForPlayers);
+            });
         }
         protected override void AbilityRemoved(Player player)
         {
             Log.Debug($"VVUP Custom Abilities: Scale Ability, Removing Scale Ability from {player.Nickname}");
-            PlayersWithScaleAbility.Remove(player);
             player.Scale = Vector3.one;
+            player.SetScale(Vector3.one);
         }
     }
 }
