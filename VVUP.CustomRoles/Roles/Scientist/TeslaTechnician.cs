47b0d4fda2cbf078e61cd39e88b1a21eef263af8
Snivy Films <120346554+SnivyFilms@users.noreply.github.com>
2025-10-24 16:48:40 -0400

    Whoopies doodles

--- VVUP.CustomRoles/Roles/Scientist/TeslaTechnician.cs
+++ VVUP.CustomRoles/Roles/Scientist/TeslaTechnician.cs
@@ -2,6 +2,7 @@
 using Exiled.API.Features.Attributes;
 using Exiled.CustomRoles.API.Features;
 using PlayerRoles;
+using VVUP.CustomRoles.Abilities.Passive;
 using VVUP.CustomRoles.API;
 
 namespace VVUP.CustomRoles.Roles.Scientist
@@ -13,16 +14,22 @@ namespace VVUP.CustomRoles.Roles.Scientist
         public override uint Id { get; set; } = 64;
         public override RoleTypeId Role { get; set; } = RoleTypeId.Scientist;
         public override int MaxHealth { get; set; } = 100;
-        public override string Name { get; set; } = "<color=#FFFF7C>Quantum Gravity Researcher</color>";
-        public override string Description { get; set; } = "A Scientist who researches quantum gravity.";
-        public override string CustomInfo { get; set; } = "Quantum Gravity Researcher";
+        public override string Name { get; set; } = "<color=#FFFF7C>Tesla Technician</color>";
+        public override string Description { get; set; } = "A Scientist who routinely works on Tesla Gates.";
+        public override string CustomInfo { get; set; } = "Tesla Technician";
 
         public override List<string> Inventory { get; set; } = new()
         {
             ItemType.KeycardScientist.ToString(),
             ItemType.Medkit.ToString(),
         };
-
-        public override string AbilityUsage { get; set; } = "You have no custom abilities";
+        public override List<CustomAbility>? CustomAbilities { get; set; } = new()
+        {
+            new IgnoreTeslas()
+            {
+                Name = "Ignore Teslas [Passive]",
+            },
+        };
+        public override string AbilityUsage { get; set; } = "You have passive abilities. This does not require button activation";
     }
 }
