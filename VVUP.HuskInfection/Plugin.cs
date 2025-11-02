2aa811a19a04d919dcb9bcb53783d660118cf7b9
Snivy Films <120346554+SnivyFilms@users.noreply.github.com>
2025-11-02 18:46:10 -0500

    What's versioning?

--- VVUP.HuskInfection/Plugin.cs
+++ VVUP.HuskInfection/Plugin.cs
@@ -22,8 +22,8 @@ namespace VVUP.HuskInfection
         public override string Name { get; } = "VVUP: Husk Infection";
         public override string Author { get; } = "Vicious Vikki";
         public override string Prefix { get; } = "VVUP.HK";
-        public override Version Version { get; } = new Version(3, 4, 3);
-        public override Version RequiredExiledVersion { get; } = new Version(9, 10, 0);
+        public override Version Version { get; } = new Version(3, 4, 4);
+        public override Version RequiredExiledVersion { get; } = new Version(9, 10, 1);
 
         public HuskInfectionEventHandlers HuskInfectionEventHandlers;
         public SsssEventHandlers SsssEventHandlers;
--- VVUP.Votes/Plugin.cs
+++ VVUP.Votes/Plugin.cs
@@ -13,8 +13,8 @@ namespace VVUP.Votes
         public override string Name { get; } = "VVUP: Votes";
         public override string Author { get; } = "Vicious Vikki";
         public override string Prefix { get; } = "VVUP.V";
-        public override Version Version { get; } = new Version(3, 4, 3);
-        public override Version RequiredExiledVersion { get; } = new Version(9, 10, 0);
+        public override Version Version { get; } = new Version(3, 4, 4);
+        public override Version RequiredExiledVersion { get; } = new Version(9, 10, 1);
 
         public override void OnEnabled()
         {
