05db9c23a863fa6c988393fafb369424f4ff3560
Snivy Films <Snivy.Films@gmail.com>
2026-01-14 15:59:49 -0500

    Removed Snowballs vs SCPs from randomly starting

--- VVUP.ServerEvents/ServerEventsEventHandlers/SnowballsVsScpsEventHandlers.cs
+++ VVUP.ServerEvents/ServerEventsEventHandlers/SnowballsVsScpsEventHandlers.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿/*using System.Collections.Generic;
 using System.Linq;
 using Exiled.API.Enums;
 using Exiled.API.Features;
@@ -128,4 +128,4 @@ namespace VVUP.ServerEvents.ServerEventsEventHandlers
             PlayersInOverwatchFromEvent.Clear();
         }
     }
-}
+}*/
