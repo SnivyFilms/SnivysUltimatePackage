d395d73236dec3089e2001daf89ae505557c8366
Snivy Films <120346554+SnivyFilms@users.noreply.github.com>
2026-01-28 09:35:54 -0500

    Updated compatibility glow descriptions.

--- VVUP.Base/Config.cs
+++ VVUP.Base/Config.cs
@@ -16,7 +16,7 @@ namespace VVUP.Base
         [Description("Enable compatibility glow effects for custom items that don't implement ICustomItemGlow")]
         public bool EnableCompatibilityGlow { get; set; } = false;
         
-        [Description("You can use this to set custom glow colors for custom items by their Custom Item ID if the plugin they are from doesnt provide a native option. Custom items from VVUP.CI will use a native system and will be ignored here. RGBA is 0-1 (decimals allowed)")]
+        [Description("You can use this to set custom glow colors for custom items by their Custom Item ID if the plugin they are from doesnt provide a native option. Custom items from VVUP.CI will use a native system and will be ignored here.")]
         public List<CustomItemGlowConfig> CustomItemGlow { get; set; } = new List<CustomItemGlowConfig>
         {
             new CustomItemGlowConfig
@@ -35,14 +35,18 @@ namespace VVUP.Base
     public class CustomItemGlowConfig
     {
         public uint CustomItemId { get; set; }
+        [Description("RGBA values should be between 0 and 1.")]
         public float R { get; set; } = 1;
         public float G { get; set; } = 1;
         public float B { get; set; } = 1;
         public float A { get; set; } = 1;
+        [Description("Glow range can be any value, but I recommend it to be between 0 and 1 in most cases.")]
         public float GlowRange { get; set; } = 0.25f;
+        [Description("Intensity should be between 0 and 1.")]
         public float Intensity { get; set; } = 0.25f;
         [Description("The options are None, Soft, and Hard for shadow types.")]
         public ICustomItemGlow.GlowShadowType ShadowType { get; set; } = ICustomItemGlow.GlowShadowType.None;
+        [Description("Offsets can be used to adjust the position of the glow. Useful for items that have their 'center' not in the center of the item, such as the E-11.")]
         public float OffsetX { get; set; } = 0f;
         public float OffsetY { get; set; } = 0f;
         public float OffsetZ { get; set; } = 0f;
